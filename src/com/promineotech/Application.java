3bf765fedbccd698aa33278f84482bb501bf03c0
Thayeralexander <thayeralexander@yahoo.com>
2024-09-17 12:21:49 -0700

    first commit being made

---
+++ src/com/promineotech/Application.java
@@ -0,0 +1,25 @@
+package com.promineotech;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My first Java Program!");
+		int age =67;
+		double accountBalance =34.67;
+		char middleintial = 'C';
+		boolean isHotOutside = true;
+		string firstname = "sam";
+		
+		System.out.println(age);
+		System.out.println(accountBalance);
+		System.out.println(middleintial);
+		System.out.println(isHotOutside);
+		
+		
+		
+//
+	}
+
+
+}
